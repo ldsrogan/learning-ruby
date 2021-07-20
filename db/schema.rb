926f2a49bda5fa34ed3ac5a8ce2be554e380d443
ldsrogan <ldsrogan@gmail.com>
2021-07-20 11:41:08 +0900

    added col to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_022113) do
+ActiveRecord::Schema.define(version: 2021_07_20_023624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_07_20_022113) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
