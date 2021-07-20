3eb66a8e09b1876d92d0f9d824dd4745dd323040
ldsrogan <ldsrogan@gmail.com>
2021-07-20 11:30:41 +0900

    user created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_064528) do
+ActiveRecord::Schema.define(version: 2021_07_20_022113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2021_07_19_064528) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
